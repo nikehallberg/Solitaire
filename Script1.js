8a644cfa72d81f0a6492d9c8feba67aaf142c900
Nike <nikehallberg9@gmail.com>
2024-11-27 11:58:15 +0100

    Add project files.

---
+++ Script1.js
@@ -0,0 +1,12 @@
+
+function generateSolitaireDeck() {
+    const suits = ['H', 'K', 'R', 'S'];
+    const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
+    const deck = [];
+}
+
+const cards = () => `C:\Users\nikeh\Desktop\Spelkort\\${suits}${ranks}.png`
+
+function shuffleDeck() {
+
+}
