b4c87bab0fe62859b5c69d35eaf4dac0711ce277
Nike <nikehallberg9@gmail.com>
2024-11-28 10:33:18 +0100

    .

--- Script1.js
+++ Script1.js
@@ -5,6 +5,23 @@ function generateSolitaireDeck() {
     const deck = [];
 }
 
+const cardList = [
+    {name: 'A', value: 1},
+    {name: 'T', value: 10},
+    {name: 'J' , value: 11},
+    {name: 'Q', value: 12 },
+    { name: 'K', value: 13 },
+    { name: '2', value: 2 },
+    { name: '3', value: 3 },
+    { name: '4', value: 4 },
+    { name: '5', value: 5 },
+    { name: '6', value: 6 },
+    { name: '7', value: 7 },
+    { name: '8', value: 8 },
+    { name: '9', value: 9 }
+
+]
+
 const cards = () => `C:\Users\nikeh\Desktop\Spelkort\\${suits}${ranks}.png`
 
 function shuffleDeck() {
